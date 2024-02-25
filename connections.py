54460ea77cdcf9e395d5e6bdb12dfd27a4d75edb
Sam Hildebrandt <shildebrandt@student.saintaug.nsw.edu.au>
2024-02-25 12:21:43 +1100

    Initialise program

---
+++ connections.py
@@ -0,0 +1,19 @@
+
+# Connections python game
+# By Sam Hildebrandt
+# Version 0.0.1
+
+def welcome():
+    print("Welcome to Connections!")
+
+
+
+
+
+
+
+
+
+# Main prog starts here
+welcome()
+    
